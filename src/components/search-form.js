fd2de28c2432b6bb178de731f04ca02a44fe7110
PeHaa <info@pehaa.com>
2020-04-07 17:06:39 +0200

    useRef

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -1,12 +1,12 @@
 /* src/components/search-form.js */
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { navigate } from "@reach/router"
 const SearchForm = ({ initialQuery = "" }) => {
   // Create a piece of state, and initialize it to initialQuery
   // query will hold the current value of the state,
   // and setQuery will let us change it
   const [query, setQuery] = useState(initialQuery)
-
+  const inputEl = useRef(null)
   // On input change use the current value of the input field (e.target.value)
   // to update the state's query value
   const handleChange = e => {
@@ -17,7 +17,8 @@ const SearchForm = ({ initialQuery = "" }) => {
   // with a query q paramenter equal to the state's query value
   const handleSubmit = e => {
     e.preventDefault()
-    navigate(`/search?q=${query}`)
+    const q = inputEl.current.value
+    navigate(`/search?q=${q}`)
   }
   return (
     <form role="search" onSubmit={handleSubmit}>
@@ -25,6 +26,7 @@ const SearchForm = ({ initialQuery = "" }) => {
         Search for:
       </label>
       <input
+        ref={inputEl}
         id="search-input"
         type="search"
         value={query}
